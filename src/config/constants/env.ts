8063492efeceb9551f2305c62f1f1d13a82d480f
cx-nil <cx060@codelogicx.com>
2025-11-07 12:23:24 +0530

    Refactor environment variable loading to use simplified dotenv import and remove unused variables from configuration.

--- src/config/constants/env.ts
+++ src/config/constants/env.ts
@@ -1,22 +1,7 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import 'dotenv/config';
 
 export const Env = {
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME,
-  DB_DIALECT: process.env.DB_DIALECT,
-
-  HOST: process.env.HOST,
-  PORT: process.env.PORT,
-
   NODE_ENV: process.env.NODE_ENV,
-
-  SWAGGER_UNAME: process.env.SWAGGER_UNAME,
-  SWAGGER_PASSWORD: process.env.SWAGGER_PASSWORD,
 
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   JWT_TOKEN_EXPIRATION: '24h',
